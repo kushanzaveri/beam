f87f2eadf6ac08036b52f91d6a0e0313b63d757e
atarw <atarw11@outlook.com>
2018-09-15 17:47:13 -0400

    drip harder

--- public/app.js
+++ public/app.js
@@ -1,4 +1,10 @@
 // reference: https://github.com/muaz-khan/RTCMultiConnection/blob/master/docs/api.md
+
+let connection = null;
+let myVideoId = null;
+let myVideo = null;
+let myRoomId = null;
+
 window.onload = () => {
     var config = {
         apiKey: "AIzaSyA8Rgk_fcVf5PjClZLqd33iOhLE8kMSpVY",
@@ -10,15 +16,16 @@ window.onload = () => {
     };
     firebase.initializeApp(config);
 
+    let canvas = document.getElementById('canvas');
+
     var userKey = "gay";
 
-    let connection = new RTCMultiConnection();
-    connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/';
+    //joinRoom('atharvas1-room');
+};
 
-    let myVideoId = null;
-    let myVideo = null;
-    let myRoomId = null;
-    let canvas = document.getElementById('canvas');
+function initializeConnection() {
+    connection = new RTCMultiConnection();
+    connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/';
 
     let options = {
         localMediaConstraints: {
@@ -36,9 +43,23 @@ window.onload = () => {
     connection.videosContainer = document.getElementById('videos-container');
     console.log("c " + connection.videosContainer);
     connection.getUserMediaHandler(options);
+}
 
-    // kushan replace 'bornas-room' with the room finding logic
-    connection.openOrJoin('atharvas1-room', (isRoomExists, roomid) => {
+function leaveRoom() {
+    // to leave entire room
+    connection.getAllParticipants().forEach(participantId => {
+        connection.disconnectWith(participantId);
+    });
+}
+
+function joinRoom(roomId) {
+    console.log('tryna join ' + roomId);
+
+    initializeConnection();
+    connection.openOrJoin(roomId, (inRoomAlready, roomid) => {
+        // leave current room
+        leaveRoom();
+
         console.log('in rm ' + roomid);
         myRoomId = roomid;
         myVideo = document.querySelector('video');
@@ -53,11 +74,10 @@ window.onload = () => {
         myVideo.ontimeupdate = () => {
             ctx.drawImage(myVideo, 0, 0, canvas.width, canvas.height);
             let data = canvas.toDataURL('image/png');
-            processImage(data);
-            // borna
+            //processImage(data);
         };
     });
-};
+}
 
 function connectPress() {
     userKey = firebase.database().ref('waiting/').push(
@@ -153,7 +173,7 @@ function processImage(dataURL) {
 
     .done(function(data) {
         // Show formatted JSON on webpage.
-        console.log(JSON.stringify(data, null, 2));
+        //document.getElementById('face-data').innerHTML = JSON.stringify(data, null, 2);
     })
 
     .fail(function(jqXHR, textStatus, errorThrown) {
